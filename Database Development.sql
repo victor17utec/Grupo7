17fc442d4126c27ca17404a2c1aeea8348215bd0
victor17utec <victor.galloni@estudiantes.utec.edu.uy>
2024-09-26 12:44:38 -0300

    Nuevas tablas, 12 en total

--- Database Development.sql
+++ Database Development.sql
@@ -1,3 +1,68 @@
+CREATE TABLE Usuarios(
+  Id_Usuario SERIAL PRIMARY KEY,
+  Nombre VARCHAR(50) NOT NULL,
+  Apellido VARCHAR(50) NOT NULL,
+  Documento CHAR(8) UNIQUE NOT NULL,
+  Correo VARCHAR(50) UNIQUE NOT NULL,
+  Calle VARCHAR(50) NOT NULL,
+  Nro_Puerta VARCHAR(10),
+  Apto VARCHAR(10),
+  Id_Perfil INT NOT NULL,
+  FOREIGN KEY (Id_Perfil) REFERENCES Perfiles (Id_Perfil)
+);
+
+CREATE TABLE Perfiles(
+  Id_Perfil SERIAL PRIMARY KEY,
+  Nom_Perfil VARCHAR(50) UNIQUE NOT NULL,
+  Estado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Perfil_Funcionalidad(
+Id_Funcionalidad SERIAL NOT NULL,
+Id_Perfil SERIAL NOT NULL,
+CONSTRAINT PK_Funcionalidad_Perfil PRIMARY KEY (Id_Funcionalidad, Id_Perfil),
+FOREIGN KEY (Id_Funcionalidad) REFERENCES Funcionalidaddes (Id_Funcionalidad),
+FOREIGN KEY (Id_Perfil) REFERENCES Perfiles (Id_Perfil)
+);
+
+CREATE TABLE Recursos(
+Id_Recurso SERIAL PRIMARY KEY,
+Descripcion VARCHAR(50) NOT NULL,
+Tar_Socio NUMERIC NOT NULL,
+Tar_No_Socio NUMERIC NOT NULL
+);
+
+CREATE TABLE Reservas(
+  Id_Reserva SERIAL PRIMARY KEY,
+  Senia NUMERIC NOT NULL,
+  Saldo NUMERIC NOT NULL,
+  Mon_Total NUMERIC NOT NULL,
+  Ca_Personas INT NOT NULL,
+  Dat_Contacto VARCHAR(20) NOT NULL,
+  Horario TIME NOT NULL,
+  Fecha DATE NOT NULL,
+  Id_Recurso INT,
+  Id_Cliente INT,
+  Id_Usuario INT,
+  FOREIGN KEY (Id_Recurso) REFERENCES Recursos(Id_Recurso),
+  FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente),
+  FOREIGN KEY (Id_Usuario) REFERENCES Usuarios(Id_Usuario)
+);
+
+CREATE TABLE Clientes(
+  Id_Usuario SERIAL PRIMARY KEY,
+  Id_Cliente SERIAL NOT NULL,
+  Id_Categoria SERIAL NOT NULL,
+  Nro_Asociado INT UNIQUE NOT NULL,
+  Pag_Cuota BOOLEAN UNIQUE NOT NULL,
+  Tot_Anual_Cuotas INT NOT NULL,
+  Uso_Leng_Senias BOOLEAN NOT NULL,
+  Dif_Auditiva BOOLEAN NOT NULL,
+  CONSTRAINT PK_Clientes PRIMARY KEY (Id_Usuario, Id_Cliente),
+  FOREIGN KEY (Id_Categoria) REFERENCES Cate_Clientes (Id_Categoria),
+  FOREIGN KEY (Id_Usuario) REFERENCES Usuarios(Id_Usuario)
+);
+
 CREATE TABLE Cuotas(
   Id_Cuota SERIAL PRIMARY KEY,
   Nro_Cuota INT UNIQUE NOT NULL,
@@ -7,6 +72,28 @@ CREATE TABLE Cuotas(
   Id_Usuario INT NOT NULL,
   CONSTRAINT FK_Cuotas_Clientes FOREIGN KEY (Id_Cliente, Id_Usuario)
   REFERENCES Clientes (Id_Cliente, Id_Usuario)
+);
+
+CREATE TABLE Cliente_Subcomision(
+  Id_Subcom INT NOT NULL,
+  Id_Cliente INT NOT NULL,
+  Id_Usuario INT NOT NULL,
+  CONSTRAINT PK_Cliente_Subcomision PRIMARY KEY (Id_Usuario, Id_Cliente),
+  FOREIGN KEY (Id_Categoria) REFERENCES Cate_Clientes (Id_Categoria),
+  FOREIGN KEY (Id_Usuario) REFERENCES Usuarios (Id_Usuario)
+);
+
+CREATE TABLE Actividades(
+  Id_Actividad SERIAL PRIMARY KEY,
+  Id_Usuario INT NOT NULL,
+  Cod_Actividad INT UNIQUE NOT NULL,
+  Nom_Actividad VARCHAR(50) NOT NULL,
+  Fec_Actividad DATE NOT NULL,
+  Hor_Actividad TIME NOT NULL,
+  Lugar VARCHAR(50) NOT NULL,
+  Duracion INT NOT NULL,
+  Cos_Ticket NUMERIC NOT NULL,
+  FOREIGN KEY (Id_Usuario) REFERENCES Usuarios (Id_Usuario)
 );
 
 CREATE TABLE Cliente_Actividad(
